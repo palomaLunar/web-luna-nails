a894e7001bb2b7c9146dde229c062a416ae69772
Paloma Luna <182629119+palomaLunar@users.noreply.github.com>
2024-10-02 14:20:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!-- Aqui vamos a mostrar el contenido principal de la pagina, es la pagina por defecto y es el apartado uñas acrilicas en nuestro menu principal  -->
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>UÑAS-ACRILICAS</title>
+    <link rel="stylesheet" href="../Controles/style.css">
+    <link rel="stylesheet" href="../Controles/bootswatch.css">
+</head>
+<body>
+   <header>
+   <?php
+include '../unas-valladolid/VISUAL/header.php';
+?>
+   </header>
+   <main></main>
+   <footer>
+   <?php
+            include '../unas-valladolid/VISUAL/footer.php';
+       ?>
+   </footer>
+</body>
+</html>
